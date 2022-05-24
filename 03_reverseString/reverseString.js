601a4cb167f3075bb269c3c8d48570eba4874dab
Will Pabs <pabiterowilliam@yahoo.com>
2022-05-24 16:22:44 -0400

    Completed exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let wordArray = word.split('');
+    let newWord = '';
+    for (let i = wordArray.length - 1; i >= 0; i--) {
+        newWord = newWord.concat(wordArray[i]);
+    }
+    return newWord;
 };
 
 // Do not edit below this line
