c763d73e0aab099fe151b5ddd87de342684fd157
Will Pabs <pabiterowilliam@yahoo.com>
2022-05-25 12:14:23 -0400

    Finished exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // if year is divisible by 4 return true
+    // if year is divisible by 100 return false
+        // unless year is divisible by 400
+    if (year % 4 === 0) {
+        if (year % 100 !== 0) return true;
+        if (year % 100 === 0 && year % 400 === 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return false;
 };
 
 // Do not edit below this line
