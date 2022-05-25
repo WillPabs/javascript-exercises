3f444c761d65a423b25d9bde6e969741bc5af5c7
Will Pabs <pabiterowilliam@yahoo.com>
2022-05-25 13:08:27 -0400

    Finished exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  array.forEach(num => sum += num);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((prev, current) => prev *= current, 1);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1**num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) return 1;
+	let count = num - 1;
+  while (count > 0) {
+    num *= count;
+    count--;
+  }
+  return num;
 };
 
 // Do not edit below this line
