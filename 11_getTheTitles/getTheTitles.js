1609a4b605908efeebba9d12bbd5674655530ad6
Will Pabs <pabiterowilliam@yahoo.com>
2022-05-25 13:14:31 -0400

    Finished exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    titlesArray = [];
+    array.forEach(book => {
+        titlesArray.push(book.title);
+    });
+    return titlesArray;
 };
 
 // Do not edit below this line
