c77d62b9f239f027087dd7cd9304ec48a9155588
Will <pabiterowilliam@yahoo.com>
2022-05-26 18:13:46 -0400

    Finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,12 @@
 const palindromes = function (string) {
     let reverse = '';
-    let array = Array.from(string);
-    for (let i = array.length - 1; i >= 0; i--) {
+    let array = Array.from(string).filter(letter => letter.toUpperCase() !== letter.toLowerCase());
+    let newString = '';
+    for (let i = array.length - 1, j = 0; i >= 0, j < array.length; i--, j++) {
         reverse = reverse.concat(array[i]);
+        newString = newString.concat(array[j]);
     }
-    return reverse === string ? true : false;
+    return reverse.toLowerCase() === newString.toLowerCase() ? true : false;
 };
 
 // Do not edit below this line
