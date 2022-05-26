115d7aed37f9efff3fcd96f185ae7dbf117dcf1e
Will Pabs <pabiterowilliam@yahoo.com>
2022-05-26 16:26:26 -0400

    Passed test 1

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let reverse = '';
+    let array = Array.from(string);
+    for (let i = array.length - 1; i >= 0; i--) {
+        reverse = reverse.concat(array[i]);
+    }
+    return reverse === string ? true : false;
 };
 
 // Do not edit below this line
