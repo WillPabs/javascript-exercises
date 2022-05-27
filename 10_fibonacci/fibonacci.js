b00f6fde12012c3d692195e9aa1e80fe37ce60be
Will <pabiterowilliam@yahoo.com>
2022-05-26 20:48:53 -0400

    Finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num == 1 || num == 2) return 1;
+    if (num === 0) return 0;
+    if (num < 0) return 'OOPS';
+    if (typeof num === 'string') num = parseInt(num);
+    let a = 1; 
+    let b = 1;
+    let sum;
+    while (num > 2) {
+        sum = a + b;
+        a = b;
+        b = sum;
+        num--;
+    }
+    return sum;
+    
 };
 
 // Do not edit below this line
