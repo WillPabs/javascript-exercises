a4283132b34060dbb79c5ad67aacae29a2409a16
Will <pabiterowilliam@yahoo.com>
2022-05-26 21:13:44 -0400

    Finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestAge = 0;
+    let oldestPerson;
+    let age = 0;
+    people.forEach(person => {
+        if (person.yearOfDeath === undefined) {
+            let currentYear = new Date().getFullYear();
+            age = currentYear - person.yearOfBirth;
+            if (age > oldestAge) {
+                oldestAge = age;
+                oldestPerson = person;
+            };
+        } else {
+            age = person.yearOfDeath - person.yearOfBirth;
+            if (age > oldestAge) {
+                oldestAge = age;
+                oldestPerson = person;
+            };
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
